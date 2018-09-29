e1c6d8902c4dc41a5eef13b5acb6a94ddd15246a
nataliagromova <ycheba.ychebnaya@gmail.com>
2018-09-29 20:53:06 +0400

    New package

--- src/main/java/first/Main.java
+++ src/main/java/first/Main.java
@@ -1,45 +1,87 @@
 package first;
 
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Find GCD of 2 numbers enter 1");
         System.out.println("Make compare enter 2");
         System.out.println("For exit enter 0");
-       while (true) {
-           int init =scanner.nextInt();
-           if (init==1) {
-               System.out.println("Enter two numbers:");
-               int a = scanner.nextInt();
-               int b = scanner.nextInt();
-               System.out.println("Here we have 3 types of Euclidean algorithm output including simple,extended and binary version: ");
-               System.out.println(Euclid.EuclidGCD(a, b));
-               System.out.println(Euclid.EulcidEXT(a, b));
-               System.out.println(Euclid.EuclidBIN(a, b));
-           } else if (init ==2) {
-               System.out.println("For compare enter k-number of moduli(not 0) then a1...ak-resudies and them m1...mk-moduli:");
-               System.out.println("Enter k");
-               int k = scanner.nextInt();
-               int[] a = new int[k];
-               int[] m = new int[k];
-               System.out.println("Enter a1..ak");
-               for (int i = 0; i < k; i++) {
-                   a[i] = scanner.nextInt();
-               }
-               System.out.println("Enter m1...mk");
-               for (int i = 0; i < k; i++) {
-                   m[i] = scanner.nextInt();
-               }
-               System.out.println("Here we have 2 output. The first one is Chinese Reminders Theorem and another is Garner's algorithm:");
-               System.out.println(Comparisons.ChineseTheorem(k, a, m));
-               System.out.println(Comparisons.GarnerMethod(k, a, m));
-           } else if (init ==0)
-               return;
-           else System.out.println("Error.Try another command:)");
-           System.out.println("You can choose new number (0,1,2):");
-       }
+        while (true) {
+            String s = scanner.nextLine();
+                int init = Integer.parseInt(s);
+            if (init == 1) {
+//                System.out.println("Enter two numbers:");
+//                int a = scanner.nextInt();
+//                int b = scanner.nextInt();
+//                System.out.println("Here we have 3 types of Euclidean algorithm output including simple,extended and binary version: ");
+//                double start = System.nanoTime();
+//                System.out.println(Euclid.EuclidGCD(a, b));
+//                double finish = System.nanoTime();
+//                System.out.println("Time " + (finish - start) / 1e9);
+//                start = System.nanoTime();
+//                System.out.println(Euclid.EulcidEXT(a, b));
+//                finish = System.nanoTime();
+//                System.out.println("Time " + (finish - start) / 1e9);
+//                start = System.nanoTime();
+//                System.out.println(Euclid.EuclidBIN(a, b));
+//                finish = System.nanoTime();
+//                System.out.println("Time " + (finish - start) / 1e9);
+                ArrayList<Pair<Integer,Integer>> mas=new ArrayList<>();
+                int l=1;
+                int ll=1000;
+                while (mas.size()<1000000){
+                    for (int j =l ; j < ll; j++) {
+                        for (int k = j; k <ll ; k++) {
+                            mas.add(new Pair<>(j,k));
+                        }
+                    }
+                }
+                double sum=0;
+                double start ;
+                double finish ;
+                for (int i = 0; i < mas.size(); i++) {
+                    start= System.nanoTime();
+                    Euclid.EuclidBIN(mas.get(i).getKey(),mas.get(i).getValue());
+                    finish= System.nanoTime();
+                    sum+=finish-start;
+                }
+                System.out.println("gcd"+(sum/mas.size()/1e9));
+
+            } else if (init == 2) {
+                System.out.println("For compare enter k-number of moduli(not 0) then a1...ak-resudies and them m1...mk-moduli:");
+                System.out.println("Enter k");
+                int k = scanner.nextInt();
+                int[] a = new int[k];
+                int[] m = new int[k];
+                System.out.println("Enter a1..ak");
+                for (int i = 0; i < k; i++) {
+                    a[i] = scanner.nextInt();
+                }
+                System.out.println("Enter m1...mk");
+                for (int i = 0; i < k; i++) {
+                    m[i] = scanner.nextInt();
+                }
+                System.out.println("Here we have 2 output. The first one is Chinese Reminders Theorem and another is Garner's algorithm:");
+                double start = System.nanoTime();
+                System.out.println(Comparisons.ChineseTheorem(k, a, m));
+                double finish = System.nanoTime();
+                System.out.println("Time " + (finish - start) /1e9);
+                start = System.nanoTime();
+                System.out.println(Comparisons.GarnerMethod(k, a, m));
+                finish = System.nanoTime();
+                System.out.println("Time " + (finish - start) /1e9);
+            } else if (init == 0)
+                return;
+            else System.out.println("Error.Try another command:)");
+            System.out.println("You can choose new number (0,1,2):");
+        }
     }
 }
---
+++ src/main/java/second/Main.java
@@ -0,0 +1,253 @@
+package second;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by NATA on 29.09.2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a number and choose needed test :\n" +
+//                "1-Theorem of Ferma\n" +
+//                "2-Solovei_Shtrassen test\n" +
+//                "3-Miller_Rabin test\n"
+//
+//        );
+//        System.out.println("Enter a number");
+
+
+//        Long number = scanner.nextLong();
+//        scanner.nextLine();
+//        while (true) {
+//
+//            String command = scanner.nextLine();
+//
+//            switch (command) {
+//                case "1":
+//                    System.out.print("Ferma test:\n" + number + " is ");
+//                    if (!Fermat(number))
+//                        System.out.println("composite");
+//                    else System.out.println("maybe prime");
+//                    break;
+//                case "2":
+//                    Solovei_Shtrassen(number);
+//                    break;
+//                case "3":
+//                    Miller_Rabin(number);
+//                    break;
+//                case "exit":
+//                    return;
+//                default:
+//                    System.out.println("Wrong command,try again.");
+//                    break;
+//            }
+//        }
+        int i=0;
+        int s1=0,s2=0,s3=0;
+        int p=6653;
+        long start = System.nanoTime();
+        long finish = System.nanoTime();
+        for (int j = 0; j <1 ; j++) {
+            while (i != 1) {
+                start=System.nanoTime();
+                if (!Fermat((long) p))
+                p=p;
+                finish=System.nanoTime();
+                s1+=(finish - start) ;
+                start=System.nanoTime();
+                if (!Solovei_Shtrassen((long) p))
+                    p=p;
+                finish=System.nanoTime();
+                s2+=(finish - start);
+                start=System.nanoTime();
+                if (!Miller_Rabin((long) p))
+                    p=p;
+                finish=System.nanoTime();
+                s3+=(finish - start) ;
+                i++;
+            }
+        }
+        System.out.println(String.format("%,1d",s1));
+        System.out.println(String.format("%,1d",s2));
+        System.out.println(String.format("%,1d",s3));
+       // System.out.println("s1=" + s1/10000 + ",s2=" + s2/10000 + ",s3=" + s3/10000);
+    }
+
+    public static boolean Fermat(Long n) {
+        Random r=new Random();
+        int a = r.nextInt(Math.toIntExact(n));
+        while (a>n || a==0&&a<n)
+            a=r.nextInt(Math.toIntExact(n));
+        while (n / a != 1)
+                a++;
+        BigInteger c = new BigInteger(String.valueOf(a));
+        if (c.modPow(BigInteger.valueOf(n - 1), BigInteger.valueOf(n)).equals(BigInteger.ONE)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean isPrime(Long n) {
+        if (n == 2)
+            return true;
+        if (n == 1 || n % 2 == 0)
+            return false;
+        Long sq = (long) Math.sqrt(n);
+        for (int i = 3; i < sq; i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static Long power(Long x, Long y, Long mod) {
+        if (y == 0)
+            return 1l;
+        Long temp = power(x, y / 2, mod) % mod;
+        temp = (temp * temp) % mod;
+        if (y % 2 == 1)
+            temp = (temp * x) % mod;
+        return temp;
+    }
+
+    public static boolean Carlmichael(Long x) {
+        if (isPrime(x))
+            return false;
+        for (int i = 2; i < x; i++) {
+            if (power(Long.valueOf(i), x, x) != i)
+                return false;
+        }
+        return true;
+    }
+
+    public static Long Jacobi(Long a, Long b) {
+        for (int i = 2; i < b; i++) {
+            if (b % i == 0) {
+                System.out.println("Number " + b + " is composite");
+                return 0L;
+            }
+        }
+        Long r = 1L;
+        if (a < 0) {
+            a = -a;
+            if (b % 4 == 3)
+                r = -r;
+        }
+        do {
+
+            int t = 0;
+            while (a % 2 == 0) {
+                t++;
+                a /= 2;
+            }
+            if (t % 2 != 0) {
+                if (b % 8 == 3 || b % 8 == 5)
+                    r = -r;
+            }
+            if (a % 4 == 3 && b % 4 == 3)
+                r = -r;
+            Long c = a;
+            a = b % c;
+            b = c;
+
+        }
+        while (a != 0);
+        return r;
+    }
+
+    public static boolean Solovei_Shtrassen(Long n) {
+    //    System.out.println("Reliability parametr is 10");
+        int t = 10;
+        int end = 0;
+        int prime = 0;
+    //    System.out.print("Solovei-Shtrassen test:\n" + n + " is ");
+        while (end != t) {
+            Long c = (long) (Math.random() * (n - 2 - 2) + 2);
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+        //            System.out.println("composite");
+                    return false;
+                }
+            }
+            if (n % c == 0 && n % 2 == 0) {
+       //         System.out.println("composite");
+                return false;
+            } else {
+                Long r = Jacobi(c, n);
+
+                BigInteger s = new BigInteger(String.valueOf(c));
+                s = s.modPow(BigInteger.valueOf((n - 1) / 2), BigInteger.valueOf(n));
+                if (BigInteger.valueOf(r).equals(s))
+                    prime++;
+            }
+            end++;
+
+        }
+        if(1 - Math.pow(2, -prime)<0.9)
+            return false;
+        else
+        return true;
+//        System.out.println("prime with probability " + (1 - Math.pow(2, -prime)));
+    }
+
+    public static boolean Miller_Rabin(Long n) {
+ //       System.out.println("Number of rounds is 100");
+
+        if (n < 2 || n % 2 == 0) {
+   //         System.out.println("Wrong number");
+            return false;
+        } else {
+  //          System.out.print("Miller_Rabin test:\n");
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+   //                 System.out.println(n + " is a composite");
+                    return false;
+                }
+            }
+            int k = 0;
+            Long t = n - 1;
+            int s = 0;
+            while (t % 2 == 0) {
+                t /= 2;
+                s++;
+            }
+            int prime = 0;
+            label:
+            while (k != 20) {
+                Long a = (long) (Math.random() * (n - 2 - 2) + 2);                   //первый цикл
+                BigInteger x = new BigInteger(String.valueOf(a));
+                x = x.modPow(BigInteger.valueOf(t), BigInteger.valueOf(n));
+                if (x.equals(BigInteger.ONE) || x.equals(BigInteger.valueOf(-1))) {
+                    prime++;
+                    k++;
+                    continue;
+                } else {
+                    for (int i = 0; i < s; i++) {                                      //второй
+                        x = x.modPow(BigInteger.valueOf(2), BigInteger.valueOf(n));
+                        if (x.equals(BigInteger.ONE)) {
+                            break;
+                        }
+                        if (x.equals(BigInteger.valueOf(-1))) {
+                            prime++;
+                            k++;
+                            continue label;
+                        }
+                    }
+
+                }
+                k++;
+            }
+            if((1 - Math.pow(4, -prime))<0.9)
+            return false;
+            else return true;
+//            System.out.print(n + " is ");
+//            System.out.printf("prime with probability %.4f ",1 - Math.pow(4, -prime));
+//            System.out.println();
+        }
+    }
+}
